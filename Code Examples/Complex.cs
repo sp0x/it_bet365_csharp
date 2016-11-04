f4a150c36a49ef6567536ecb4dfc5ab088c633a9
Sp0x <sp0x@xakep.ru>
2016-11-04 10:42:25 +0200

    Changed complex example

--- Code Examples/Complex.cs
+++ Code Examples/Complex.cs
@@ -16,7 +16,7 @@ namespace app2
         {
             this.mReal = mReal;
             this.mImaginary = mImaginary;
-        } 
+        }
         /// <summary>
         /// Override the + operator to add 2 complex numbers
         /// </summary>
@@ -37,6 +37,7 @@ namespace app2
         {
             return new Complex(c1.mReal - c2.mReal, c1.mImaginary - c2.mImaginary);
         }
+        
         /// <summary>
         /// Use the default equals
         /// </summary>
@@ -47,6 +48,23 @@ namespace app2
         {
             return c1.Equals(c2);
         }
+
+        /// <summary>
+        /// By overloading > you should also overlad <
+        /// </summary>
+        /// <param name="c1"></param>
+        /// <param name="c2"></param>
+        /// <returns></returns>
+        public static bool operator >(Complex c1, Complex c2)
+        {
+            return false;
+        }
+        public static bool operator <(Complex c1, Complex c2)
+        {
+            return false;
+        }
+         
+
         /// <summary>
         /// By overloading == you should also override the != operator
         /// </summary>
@@ -62,13 +80,13 @@ namespace app2
         /// </summary>
         /// <param name="t"></param>
         /// <returns></returns>
-        public static bool operator true(Complex t) { return t.mReal != 0; }
+        public static bool operator true(Complex t) { return t.mReal != 0 && t.mImaginary!=0; }
         /// <summary>
         /// Overrides the false value of complex, in case of if(!complexNumber) {}
         /// </summary>
         /// <param name="t"></param>
         /// <returns></returns>
-        public static bool operator false(Complex t) { return t.mReal == 0; }
+        public static bool operator false(Complex t) { return t.mReal == 0 && t.mImaginary == 0; }
 
         /// <summary>
         /// Return a string with the complex number
